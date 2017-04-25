f9d629a4ccf4ee74ec16823608e0ab1c554d9ec3
Moonlock <moonlock@riseup.net>
2017-04-24 20:26:18 -0600

    Made font in graphs bigger.

--- histogram.m
+++ histogram.m
@@ -11,29 +11,46 @@ function histogram(red, yellow, combined, plotTitle, names=[], groups=false)
     type = "hist";
   else
     type = "stacked";
-  end
-   
-%  graphics_toolkit ("fltk");
+  end 
+  fontSize = 30;
+
+  graphics_toolkit ("fltk");
+  
   figure(1);
   set(gcf,'NumberTitle','off');
   set(gcf, 'Name', plotTitle);
   
   subplot(2, 2, 1);
-  bar(red', type);
+
+  if(rows(names) == 1 || groups)
+    bar(red', type);
+  else
+    bar(red', type, "facecolor", "red");
+  end
+  set(gca, "fontsize", fontSize)
   axis([-Inf Inf 0 yMax]);
   xlabel("Red Die");
   ylabel("Frequency");
   
   subplot(2, 2, 2);
-  bar(yellow', type);
+
+  if(rows(names) == 1 || groups)
+    bar(yellow', type);
+  else
+    bar(yellow', type, "facecolor", "yellow");
+  end
+  set(gca, "fontsize", fontSize)
   axis([-Inf Inf 0 yMax]);
   xlabel("Yellow Die");
   ylabel("Frequency");
   
   subplot(2, 2, [3 4]);
   bar([2 3 4 5 6 7 8 9 10 11 12], combined', type);
+  set(gca, "fontsize", fontSize)
   xlabel("Combined Dice");
   ylabel("Frequency");
   legend(names', "location", 'northoutside', 'orientation', 'horizontal');
   
 endfunction
+
+
