e5c2715c64aededf12a39100ef4d220553407721
Moonlock <moonlock@riseup.net>
2017-05-14 20:31:29 -0600

    Surround emailing in try/except block.

--- diceTest.py
+++ diceTest.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 import json
 import os
 import smtplib
+import socket
 
 from blessings import Terminal
 from oct2py import octave
@@ -193,9 +194,9 @@ def displayHelp():
 		"	Results of both dice combined")
 	print("	" + term.underline("s") + "eparate [player | 'all']" +
 		"	Results of dice separately")
-	print("	" + term.underline("g") + "raph    [player | 'all']" +
+	print("	" + term.underline("g") + "raph [player | 'all']" +
 		"	Graph results")
-	print("	" + term.underline("g") + "raph    ['on' | 'off']" +
+	print("	" + term.underline("g") + "raph	['on' | 'off']" +
 		"	Update graph after every roll")
 	print("")
 	print("	" + term.underline("n") + "ext				" +
@@ -364,35 +365,46 @@ def saveResults():
 	print("Results saved.")
 	
 def emailResults():
-	mailserver = smtplib.SMTP(config.HOST, 587)
-	mailserver.ehlo()
-	mailserver.starttls()
-	mailserver.ehlo()
-	mailserver.login(config.ADDRESS, config.PASSWORD)
+	try:
+		tryEmailResults()
+	except socket.gaierror, smtplib.SMTPServerDisconnected:
+		print("No internets.")
+
+		response = input("Try again? ").lower()
+		while not (response == 'n' or response == 'no'):
+			if response == 'y' or response == 'yes':
+				emailResults()
+			response = input("Try again? [y/n] ").lower()
+
+def tryEmailResults():
+	with smtplib.SMTP(config.HOST, 587) as mailserver:
+		mailserver.ehlo()
+		mailserver.starttls()
+		mailserver.ehlo()
+		mailserver.login(config.ADDRESS, config.PASSWORD)
 	
-	addresses = []
+		addresses = []
 
-	print("Enter email addresses (blank line to end):")
-	response = input(" > ")
-	while response:
-		addresses.append(response)
+		print("Enter email addresses (blank line to end):")
 		response = input(" > ")
+		while response:
+			addresses.append(response)
+			response = input(" > ")
 
-	fp = file(filename)
-	data = fp.read()
-	attachment = MIMEText(data)
-	attachment.add_header('Content-Disposition', 'attachment', filename=filename)
-	fp.close()
+		fp = file(filename)
+		data = fp.read()
+		attachment = MIMEText(data)
+		attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+		fp.close()
 
-	msg = MIMEMultipart('alternative')
-	msg.attach(attachment)
-	msg['Subject'] = 'Dice results - ' + filename
-	msg['From'] = config.ADDRESS
-	msg['To'] = ",".join(addresses)
+		msg = MIMEMultipart('alternative')
+		msg.attach(attachment)
+		msg['Subject'] = 'Dice results - ' + filename
+		msg['From'] = config.ADDRESS
+		msg['To'] = ",".join(addresses)
 
-	mailserver.sendmail(config.ADDRESS, addresses, msg.as_string())
-	mailserver.quit()
-	print("Emails sent.")
+		mailserver.sendmail(config.ADDRESS, addresses, msg.as_string())
+		print("Emails sent.")
 
 def getGameDirectory():
 	print("")
@@ -519,8 +531,6 @@ preventYakov()
 promptToLoad()
 setup()
 start()
-
-
 
 
 
